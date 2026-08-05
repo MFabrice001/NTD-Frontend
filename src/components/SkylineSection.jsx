acf3a998a686ca1fc315382699aa78073df20505
Musengimana Fabrice <m.fabrice001@gmail.com>
2026-08-05 12:07:35 +0200

    added some pages

--- src/components/SkylineSection.jsx
+++ src/components/SkylineSection.jsx
@@ -16,7 +16,7 @@ const SkylineSection = () => {
           <img src="https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=2069&auto=format&fit=crop" alt="Modern Architecture" className="skyline-img" />
         </div>
         <div className="card">
-          <img src="https://images.unsplash.com/photo-1541888946425-d0ebb18086f6?q=80&w=2070&auto=format&fit=crop" alt="Engineering Works" className="skyline-img" />
+          <img src="https://images.unsplash.com/photo-1504307651254-35680f356f12?q=80&w=2070&auto=format&fit=crop" alt="Engineering Works" className="skyline-img" />
         </div>
         <div className="card">
           <img src="https://images.unsplash.com/photo-1600585154340-be6161a56a0c?q=80&w=2070&auto=format&fit=crop" alt="Luxury Residence" className="skyline-img" />
